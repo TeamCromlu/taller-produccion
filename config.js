4d7482d9c19f6685374425a83c9dd659dd47f2af
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2016-03-26 21:08:14 -0500

    archivo config para deploy

---
+++ config.js
@@ -0,0 +1,19 @@
+var config = {
+	twitter: {
+		key: 'CIOzO0YHEXEZVkVvUlISF6ML8',
+		secret: 'p48SSuOX45FNtmAc7FwOLAkOQ1UOOSIUclyCUOZKzm76IEKUoV'
+	},
+	facebook:{
+		id: '970993752974427',
+		secret: '587d75ac7e535144ebb6e5b7911c8fa2'
+	},
+	admin:{
+		user : 'cromlu',
+		pass : 'cromlu1221'
+	},
+	mongodb:{
+		data_connect: 'mongodb://cromlu:cromlu1221@ds057934.mongolab.com:57934/taller'
+	}
+};
+
+module.exports = config;
